11d4e1d4f3b0b77163bfaa5fef81076fdf88862e
Łukasz Kaczmarski <lukasz.kaczmarski93@gmail.com>
2016-10-27 15:47:58 +0200

    Zabezpieczenie

--- Zad1D/src/com/company/Main.java
+++ Zad1D/src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import jdk.internal.util.xml.impl.Input;
+
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
@@ -9,30 +12,38 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-	    int tries;
-        int max;
-        int min;
+	    int tries = 0;
+        int max = 0;
+        int min = 0;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj MIN");
-        min = scanner.nextInt();
+        try{
+            System.out.println("Podaj MIN");
+            min = scanner.nextInt();
 
-        System.out.println("Podaj MAX");
-        max = scanner.nextInt();
+            System.out.println("Podaj MAX");
+            max = scanner.nextInt();
 
-        System.out.println("Podaj liczbe prób");
-        tries = scanner.nextInt();
+            System.out.println("Podaj liczbe prób");
+            tries = scanner.nextInt();
+        }
+        catch (InputMismatchException ex){
+            System.out.println("Podana wartosc nie jest liczba");
+            return;
+        }
+
         int minZakres = min;
         int maxZakres = max*10;
         int counter = 0;
         for (int i = 0; i <= tries; i++) {
             int number = ThreadLocalRandom.current().nextInt(minZakres, maxZakres + 1);
+            System.out.println("Wylosowana liczba : " +number);
 
             if(number < max){
                 counter++;
             }
         }
-        System.out.println("Liczba zmiescila sie w przedziale : " + counter + " razy");
+        System.out.println("Ilosc liczb ktore zmiescily sie w przedziale : " +counter);
 
 
 
